a025416252d84db047376fb846facb91a2c423cb
Josh Davis <josh@joshldavis.com>
2014-02-02 17:46:40 +0000

    Add complete hello world

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+DEBUG_MODE = True
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
 
 if __name__ == "__main__":
+    app.debug = DEBUG_MODE
     app.run()
