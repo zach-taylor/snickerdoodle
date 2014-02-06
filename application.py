ea53af5f012a41e2cbd6f21776cf1bce75efca67
Josh Davis <josh@joshldavis.com>
2014-02-06 03:44:06 +0000

    Add /connect url

--- application.py
+++ application.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 DEBUG_MODE = True
+
 
 @app.route("/")
 def hello():
     return render_template('home.html')
 
+
+@app.route('/connect')
+def connect():
+    return render_template('connect.html')
+
+
 if __name__ == "__main__":
     app.debug = DEBUG_MODE
-    app.run()
+    app.run(host='snickerdoodle.com')
