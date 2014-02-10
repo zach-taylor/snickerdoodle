c196261fcf5de0577d745f90576cad921609df7a
Zach Taylor <ztaylor234@gmail.com>
2014-02-10 05:18:54 +0000

    Starting to build the models and set up flask-migrate. not sure how to import the models and create the database+tables yet.

--- application.py
+++ application.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost/snickerdoodle'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 DEBUG_MODE = True
+
+
 
 
 @app.route("/")
@@ -17,4 +29,4 @@ def connect():
 
 if __name__ == "__main__":
     app.debug = DEBUG_MODE
-    app.run(host='snickerdoodle.com')
+    manager.run(host='snickerdoodle.com')
