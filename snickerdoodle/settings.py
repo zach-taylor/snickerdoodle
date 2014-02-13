b6a31bba7d8d0ef2aaae33d29eee87f993d7e996
Zach Taylor <ztaylor234@gmail.com>
2014-02-13 00:07:29 +0000

    import settingslocal.py if any

--- snickerdoodle/settings.py
+++ snickerdoodle/settings.py
@@ -3,3 +3,8 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG_MODE = True
 SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost/snickerdoodle'
+
+try:
+    from settingslocal import *
+except ImportError:
+    pass
