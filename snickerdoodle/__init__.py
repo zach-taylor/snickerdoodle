46d615c787f39784a6518c8ab8660e0f27bfffee
Zach Taylor <ztaylor234@gmail.com>
2014-02-23 22:39:41 +0000

    Added facebook id column to users table. add route /user to create a new user using a json request. added some basic error handling as well

--- snickerdoodle/__init__.py
+++ snickerdoodle/__init__.py
@@ -1,6 +1,6 @@
 from os import path
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, abort, make_response
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -24,17 +24,50 @@ from users.models import User
 from rooms.models import Room
 from videos.models import Video
 
+@snickerdoodle.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
-@snickerdoodle.route("/")
+@snickerdoodle.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+@snickerdoodle.errorhandler(500)
+def internal_server_error(error):
+    return make_response(jsonify( { 'error': 'Internal server error' } ), 500)
+
+@snickerdoodle.route('/')
 def hello():
     return render_template('home.html')
 
 
-@snickerdoodle.route("/connect")
+@snickerdoodle.route('/connect')
 def connect():
     return render_template('connect.html')
 
 
-@snickerdoodle.route("/video")
+@snickerdoodle.route('/video')
 def video():
     return render_template('video.html')
+
+
+@snickerdoodle.route('/user', methods=['PUT', 'POST'])
+def new():
+    if not request.json or not 'user' in request.json:
+        abort(400)
+    try:
+        data = request.get_json(force=True)
+        user = User(data['user']['fb_id'])
+        db.session.add(user)
+        db.session.commit()
+    except Exception, e:
+        snickerdoodle.logger.warning(e)
+        abort(500)
+
+    user_json = {
+        'id': user.id,
+        'fb_id': user.fb_id
+    }
+
+    return jsonify( { 'user': user_json } ), 201
+
--- snickerdoodle/users/models.py
+++ snickerdoodle/users/models.py
@@ -3,10 +3,10 @@ from snickerdoodle import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    fb_oauth = db.Column(db.String(255))
+    fb_id = db.Column(db.String(255), unique = True)
 
-    def __init__(self, fb_oauth=None):
-        self.fb_oauth = fb_oauth
+    def __init__(self, fb_id=None):
+        self.fb_id = fb_id
 
     def __repr__(self):
-        return '<User %r>' % (self.fb_oauth)
+        return '<User %r>' % (self.fb_id)
