68e751b48778f89da212d1460257dadc261ae1b5
Josh Davis <josh@joshldavis.com>
2014-03-01 17:29:51 +0000

    Remove tabs....................

--- static/js/videoParsing.js
+++ static/js/videoParsing.js
@@ -1,14 +1,15 @@
-        function addVideo(SiteURL) {
-        var done = false;
-	var videoURL = SiteURL
-        var videoSite;
-        var videoID;
-	if (videoURL.split(".",2)[1] == "youtube") {
-            videoSite = videoURL.split(".",2)[1];
-            videoID = videoURL.split("=",2)[1];
-	} else if (videoURL.split("/",4)[2] == "vimeo.com") {
-            videoSite = videoURL.split("/",4)[2];
-            videoID = videoURL.split("/",4)[3];
-	}
-        return videoID;
-	}
+function addVideo(SiteURL) {
+    var done = false;
+    var videoURL = SiteURL
+    var videoSite;
+    var videoID;
+    if (videoURL.split(".",2)[1] == "youtube") {
+        videoSite = videoURL.split(".",2)[1];
+        videoID = videoURL.split("=",2)[1];
+    } else if (videoURL.split("/",4)[2] == "vimeo.com") {
+        videoSite = videoURL.split("/",4)[2];
+        videoID = videoURL.split("/",4)[3];
+        alert(videoID);
+    }
+    return videoID;
+}
