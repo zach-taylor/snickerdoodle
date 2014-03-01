ab22cf5f135efb8e8360fb6ab2cf8d8b2bbec484
Josh Davis <josh@joshldavis.com>
2014-03-01 17:28:56 +0000

    Add core video javascript file

--- static/js/snickerdoodle.js
+++ static/js/snickerdoodle.js
@@ -7,15 +7,19 @@
         snicker.providers[name] = provider;
     };
 
-    snicker.parseURL = function (url) {
+    snicker.parseUrl = function (url) {
+        console.log('Providers: ');
+        console.debug(snicker.providers);
+
         for (var name in snicker.providers) {
             var provider = snicker.providers[name];
 
+            console.log('Provider: ' + name);
             if (!snicker.providers.hasOwnProperty(name)) continue;
 
             // Determine if the provider can handle the given URL
             if (provider.checkUrl(url) == true) {
-                return provider;
+                return new provider(url);
             }
         }
 
@@ -23,6 +27,30 @@
         return undefined;
     };
 
+    snicker.emit = function (action, status) {
+        // Send a message to the server using Socket.io
+    };
+
     // Create our root object
-    root.snickerdoodle = snicker;
+    root.Snicker = snicker;
+
+    // Helper for hasOwnProperty
+    root.__hasProp = Object.prototype.hasOwnProperty;
+
+    // Helper function from CofeeScript for extending a "class"
+    root.__extends = function(child, parent) {
+        for (var key in parent) {
+            if (root.__hasProp.call(parent, key)) child[key] = parent[key];
+        }
+
+        function ctor() {
+            this.constructor = child;
+        }
+
+        ctor.prototype = parent.prototype;
+        child.prototype = new ctor;
+        child.__super__ = parent.prototype;
+
+        return child;
+    };
 }(window, jQuery));
