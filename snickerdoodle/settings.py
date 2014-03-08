4b84b06d0dbe5817fbc6cf9aa52ecf98c15315ad
Josh Davis <josh@joshldavis.com>
2014-03-08 03:58:44 +0000

    Use .get on dict so we can use default values

--- snickerdoodle/settings.py
+++ snickerdoodle/settings.py
@@ -5,8 +5,8 @@ DEBUG_MODE = True
 SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost/snickerdoodle'
 
 #Facebook OAuth variables
-#OAUTH_CONSUMER_KEY = os.environ['OAUTH_CONSUMER_KEY']
-#OAUTH_CONSUMER_SECRET = os.environ['OAUTH_CONSUMER_SECRET']
+OAUTH_CONSUMER_KEY = os.environ.get('OAUTH_CONSUMER_KEY', '')
+OAUTH_CONSUMER_SECRET = os.environ.get('OAUTH_CONSUMER_SECRET', '')
 
 try:
     from settingslocal import *
