6c9a6918e1219523a625295b88d8e01afe1d5f1e
Josh Davis <josh@joshldavis.com>
2014-03-09 05:45:59 +0000

    Update User model with accompanying migrations

--- snickerdoodle/users/models.py
+++ snickerdoodle/users/models.py
@@ -3,12 +3,23 @@ from snickerdoodle import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
+
+    id = db.Column(db.Integer, primary_key=True)
     display_name = db.Column(db.String(120))
-    fb_id = db.Column(db.String(255), unique = True)
+    fb_id = db.Column(db.String(255), unique=True)
+    fb_username = db.Column(db.String(255))
+    oauth_token = db.Column(db.Text())
 
     def __init__(self, fb_id=None):
         self.fb_id = fb_id
 
     def __repr__(self):
         return '<User %r>' % (self.fb_id)
+
+    @staticmethod
+    def add_user(user):
+        db.session.add(user)
+
+    @staticmethod
+    def commit():
+        db.session.commit()
