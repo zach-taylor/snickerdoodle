3cbd9be4309bdd393f1f02f626ae8468e05e64dd
Josh Davis <josh@joshldavis.com>
2014-03-11 22:34:42 +0000

    BE GONE WHITESPACE DEMONS!

--- static/js/chat.js
+++ static/js/chat.js
@@ -5,25 +5,25 @@ $(document).ready(function(){
    var io = socketio.listen(server);
    var socket = io.connect("http://127.0.0.1:5000/chat")
 
-	socket.on('Response', funciton(msg) {
-	    	$('#log').append('Received : ' + msg.data);
-	});
+    socket.on('Response', funciton(msg) {
+            $('#log').append('Received : ' + msg.data);
+    });
 
-	socket.on('Response', funciton(msgs) {
-	    	$('#log2').append('Received : ' + msgs.data);
-	});
+    socket.on('Response', funciton(msgs) {
+            $('#log2').append('Received : ' + msgs.data);
+    });
 
 
-	$('form#reply').submit(function(event){
-		socket.emit('my reply event', {data: $('#reply_data').val()});
-		return false;
-	});
+    $('form#reply').submit(function(event){
+        socket.emit('my reply event', {data: $('#reply_data').val()});
+        return false;
+    });
 
-	 $('form#reply2').submit(function(event){
-	   	socket.emit('Reply Event', {data: $('#replymsg').val()});
-	   	return false;
-	});
-	
+     $('form#reply2').submit(function(event){
+        socket.emit('Reply Event', {data: $('#replymsg').val()});
+        return false;
+    });
+
 });
 
 
