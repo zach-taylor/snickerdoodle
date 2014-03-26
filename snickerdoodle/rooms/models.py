43f64e19404b0178663ef5d576ca9055d380a925
Zach Taylor <ztaylor234@gmail.com>
2014-03-26 23:56:36 +0000

    Add room model relationship in order to get videos in a room

--- snickerdoodle/rooms/models.py
+++ snickerdoodle/rooms/models.py
@@ -5,10 +5,11 @@ class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     host_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    videos = db.relationship('Video', backref = 'room', lazy = 'dynamic')
 
     def __init__(self, name=None, host_id=None):
         self.name = name
         self.host_id = host_id
 
     def __repr__(self):
-        return '<Room %r>' % (self.name)
+        return '<Room %r>' % (self.id)
