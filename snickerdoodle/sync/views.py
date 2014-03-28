e3c3db3d1cf50f4995e257f873a0c66812bc04ea
Zach Taylor <ztaylor234@gmail.com>
2014-03-28 01:00:12 +0000

    When a video add event is triggered, the video is added to the database for later retrieval now. frontend still needs to send the provider's name with the message.

--- snickerdoodle/sync/views.py
+++ snickerdoodle/sync/views.py
@@ -1,9 +1,19 @@
 from flask.ext import socketio
 
+from ..videos.models import Video
+
+from ..extensions import db
+
 
 def video_message(data):
     print data
     socketio.emit('player', data, namespace='/video', broadcast=True)
+
+    # Add the video to the database
+    if data['action'] == 'change':
+        video = Video('', data['id'])
+        db.session.add(video)
+        db.session.commit()
 
 
 def attach_views_with_socket(app, socket):
