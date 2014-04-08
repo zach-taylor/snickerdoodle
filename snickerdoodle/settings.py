97edc7db790459b117ee97bb79d856f93eb6fd90
Zach Taylor <ztaylor234@gmail.com>
2014-04-08 02:22:11 +0000

    Fixed settings.py, I think?

--- snickerdoodle/settings.py
+++ snickerdoodle/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = '512d190ec56d834bc3bb86aa29414bf3319cf8ab'
 # Database Settings
 #
 
-
+if not os.environ.has_key('DATABASE_URL'):
+    os.environ['DATABASE_URL'] = 'postgres://postgres@localhost/snickerdoodle'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 #
