07067bd6b1cb66ad04c28972ea2985cb9153b448
Josh Davis <josh@joshldavis.com>
2014-04-28 02:20:43 +0000

    Select room name when button clicked

--- static/js/home.js
+++ static/js/home.js
@@ -1,8 +1,11 @@
 (function (root, $) {
     var $modal = $('.ui.modal'),
-        $create = $('#js-create-room-button');
+        $create = $('#js-create-room-button'),
+        $input = $('#name');
 
     $create.click(function () {
-        $modal.modal('show');
+        $modal.modal('show', function () {
+            $input.focus();
+        }).modal('show');
     });
 }(window, jQuery));
